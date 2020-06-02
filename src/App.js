81dcfe8b011a141fa3b2e107a4ecdcf43cc5cb9f
rkriar <ramneekriar@gmail.com>
2020-06-02 00:07:30 -0400

    add button adds item to list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+
+  updateInput(key, value){
+    //update react state
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addItem(){
+    //create item with unique id
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy of current list of items
+    const list = [...this.state.list];
+
+    //add new item to list
+    list.push(newItem);
+
+    //update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+
+  render(){
   return (
     <div className="App">
-      Hello World!
+      <div>
+        Add an Item...
+        <br/>
+        <input
+        type="text"
+        placeholder="Type item here..."
+        value={this.state.newItem}
+        onChange={e => this.updateInput("newItem", e.target.value)}
+        />
+        <button
+        onClick={() => this.addItem()}
+        >
+          Add
+        </button>
+      </div>
     </div>
   );
 }
+}
+
 
 export default App;
